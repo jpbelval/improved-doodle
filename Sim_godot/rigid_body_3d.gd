455bd99400d536fcb556a9a6dc5242d6f1531708
Luka Lepine <117468514+DifficultCupid2@users.noreply.github.com>
2025-10-20 15:58:21 -0400

    Creation de fonction de deplacement

--- Sim_godot/rigid_body_3d.gd
+++ Sim_godot/rigid_body_3d.gd
@@ -1,22 +1,26 @@
 extends Node3D
 
-var movementSpeed = 0.3
-
-var angle = 0.0
+var movementSpeed
+var wheelAngleDeg
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	movementSpeed = 0.5
+	wheelAngleDeg = -10.0
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	angle = rotation_degrees.y
-	position.x += movementSpeed * delta * cos(deg_to_rad(angle))
-	position.z += movementSpeed * delta * -sin(deg_to_rad(angle))
-	rotate_y(deg_to_rad(0.4))
+	move(delta)
+	pass
+	
+func move(delta: float) -> void:
+	var angle = wheelAngleDeg * delta * movementSpeed
+	rotate_y(deg_to_rad(angle))
+	position.x += movementSpeed * delta * cos(deg_to_rad(rotation_degrees.y))
+	position.z += movementSpeed * delta * -sin(deg_to_rad(rotation_degrees.y))
 	pass
 
-
-func lineFolower() -> void:
-	var i = 0
+func turn() -> void:
 	pass
+	
