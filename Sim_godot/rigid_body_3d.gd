f741d094af0634dbe5a4a03efd700bc1319664b9
Luka Lepine <117468514+DifficultCupid2@users.noreply.github.com>
2025-10-20 16:09:35 -0400

    Movement test

--- Sim_godot/rigid_body_3d.gd
+++ Sim_godot/rigid_body_3d.gd
@@ -5,12 +5,13 @@ var wheelAngleDeg
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	movementSpeed = 0.5
-	wheelAngleDeg = -10.0
+	movementSpeed = 0.25
+	wheelAngleDeg = -45.0
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	lineFollower()
 	move(delta)
 	pass
 	
@@ -20,7 +21,10 @@ func move(delta: float) -> void:
 	position.x += movementSpeed * delta * cos(deg_to_rad(rotation_degrees.y))
 	position.z += movementSpeed * delta * -sin(deg_to_rad(rotation_degrees.y))
 	pass
-
-func turn() -> void:
-	pass
 	
+func lineFollower() -> void:
+	if rotation_degrees.y > 45:
+		wheelAngleDeg = -80
+	else:
+		wheelAngleDeg = 80
+	pass
