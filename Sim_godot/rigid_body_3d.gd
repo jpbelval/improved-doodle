f223f21402979bf85a9d78403d27b0f0e14a7761
Luka Lepine <117468514+DifficultCupid2@users.noreply.github.com>
2025-10-21 10:56:20 -0400

    Basic movement

--- Sim_godot/rigid_body_3d.gd
+++ Sim_godot/rigid_body_3d.gd
@@ -1,12 +1,24 @@
 extends Node3D
 
+const maxAcc = 0.2 # m/s2
+const maxDec = -0.2 # m/s2
+const maxTurn = 45.0 # deg/s
+
 var movementSpeed
-var wheelAngleDeg
+var wheelAngle
+
+var wheelAngleTarget
+var speedTarget
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	movementSpeed = 0.25
-	wheelAngleDeg = -45.0
+	# Private
+	movementSpeed = 0.0 # m/s
+	wheelAngle = 0.0 # deg
+
+	# Public
+	wheelAngleTarget = 0.0 # deg
+	speedTarget = 0.1 # m/s
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,15 +28,46 @@ func _process(delta: float) -> void:
 	pass
 	
 func move(delta: float) -> void:
-	var angle = wheelAngleDeg * delta * movementSpeed
-	rotate_y(deg_to_rad(angle))
+	# Speed update
+	if movementSpeed < speedTarget:
+		if movementSpeed + maxAcc * delta > speedTarget:
+			movementSpeed = speedTarget
+		else:
+			movementSpeed += maxAcc * delta
+	elif movementSpeed > speedTarget:
+		if movementSpeed + maxDec * delta < speedTarget:
+			movementSpeed = speedTarget
+		else:
+			movementSpeed += maxDec * delta
+	
+	# Wheel angle update
+	if wheelAngle < wheelAngleTarget:
+		if wheelAngle + maxTurn * delta > wheelAngleTarget:
+			wheelAngle = wheelAngleTarget
+		else:
+			wheelAngle += maxTurn * delta
+	elif wheelAngle > wheelAngleTarget:
+		if wheelAngle - maxTurn * delta < wheelAngleTarget:
+			wheelAngle = wheelAngleTarget
+		else:
+			wheelAngle -= maxTurn * delta
+	
+	# Turn
+	rotate_y(deg_to_rad(wheelAngle * delta * movementSpeed))
+	
+	# Move
 	position.x += movementSpeed * delta * cos(deg_to_rad(rotation_degrees.y))
 	position.z += movementSpeed * delta * -sin(deg_to_rad(rotation_degrees.y))
 	pass
 	
 func lineFollower() -> void:
-	if rotation_degrees.y > 45:
-		wheelAngleDeg = -80
+	print('movement')
+	print([movementSpeed, speedTarget])
+	print('turnAngle')
+	print([wheelAngle, wheelAngleTarget])
+	if movementSpeed != speedTarget:
+		wheelAngleTarget = 150
+		speedTarget = 0.34
 	else:
-		wheelAngleDeg = 80
+		wheelAngleTarget = 0
 	pass
