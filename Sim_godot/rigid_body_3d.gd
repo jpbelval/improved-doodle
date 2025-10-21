931051a873bc8cc47abaf0ff985844ccee273f44
Luka Lepine <117468514+DifficultCupid2@users.noreply.github.com>
2025-10-21 17:05:14 -0400

    Line Follower update

--- Sim_godot/rigid_body_3d.gd
+++ Sim_godot/rigid_body_3d.gd
@@ -1,31 +1,50 @@
 extends Node3D
 
-@onready var color_ray = $ColorRay
+@onready var color_ray1 = $ColorRay1
+@onready var color_ray2 = $ColorRay2
+@onready var color_ray3 = $ColorRay3
 
+# Max constantes
 const maxAcc = 0.3 # m/s2
 const maxDec = -0.3 # m/s2
-const maxTurn = 120.0 # deg/s
+const maxTurn = 150.0 # deg/s
+const maxAngle = 90.0 # deg
 
+# Speed constantes
+const fullSpeed = 0.25
+const slowSpeed = 0.15
+const panicSpeed = 0.1
+
+# Angle constantes
+const straightAngle = 32
+const curveAngle = 80
+const panicAngle = maxAngle
+
+# Variables
 var movementSpeed
 var wheelAngle
 
 var wheelAngleTarget
 var speedTarget
 
+var state # State Machine
+var lastDirection # 0 : Left, 1 : Right
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Private
 	movementSpeed = 0.0 # m/s
 	wheelAngle = 0.0 # deg
+	state = 0
 
 	# Public
 	wheelAngleTarget = 0.0 # deg
-	speedTarget = 0.1 # m/s
+	speedTarget = 0.2 # m/s
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	lineFollower()
+	lineFollower(delta)
 	move(delta)
 	pass
 	
@@ -62,16 +81,65 @@ func move(delta: float) -> void:
 	position.z += movementSpeed * delta * -sin(deg_to_rad(rotation_degrees.y))
 	pass
 	
-func lineFollower() -> void:
-	if onLine():
-		wheelAngleTarget = 75.0
-	else:
-		wheelAngleTarget = -75.0
+func lineFollower(delta: float) -> void:
+	match state:
+		0: # Follow Line
+			var detection = onLine()
+			print(detection)
+			# The line as to be on the right side of the car
+			# [1, 1, 1] -> Go little left and full speed
+			if detection[0] && detection[1] && detection[2]:
+				wheelAngleTarget = -straightAngle
+				speedTarget = fullSpeed
+				lastDirection = 0
+			# [0, 0, 0] -> Depend on last direction and slow down
+			elif !detection[0] && !detection[1] && !detection[2]:
+				speedTarget = panicSpeed
+			# [0, 0, 1] -> Go right and slow down
+			elif !detection[0] && !detection[1] && detection[2]:
+				wheelAngleTarget = -curveAngle
+				speedTarget = slowSpeed
+				lastDirection = 1
+			# [0, 1, 0] -> Go little left and full speed
+			elif !detection[0] && detection[1] && !detection[2]:
+				wheelAngleTarget = straightAngle
+				speedTarget = fullSpeed
+				lastDirection = 0
+			# [0, 1, 1] -> Go little right and full speed
+			elif !detection[0] && detection[1] && detection[2]:
+				wheelAngleTarget = -straightAngle
+				speedTarget = fullSpeed
+				lastDirection = 1
+			# [1, 0, 0] -> Go left and slow down
+			elif detection[0] && !detection[1] && !detection[2]:
+				wheelAngleTarget = curveAngle
+				speedTarget = slowSpeed
+				lastDirection = 0
+			# [1, 1, 0] -> Go little left and full speed
+			elif detection[0] && detection[1] && !detection[2]:
+				wheelAngleTarget = straightAngle
+				speedTarget = fullSpeed
+				lastDirection = 0
+		
+		2: # Obstacle detected
+			var i = 0
+		3: # Go arround
+			var i = 0
+		4: # Find line
+			var i = 0
+		5: # Allign
+			var i = 0
 	pass
 	
-func onLine() -> bool:
-	if color_ray.is_colliding():
-		if color_ray.get_collider().name == "parcoursBody":
-			return true
-		return false
-	return false
+func onLine() -> Array:
+	var detection = [0, 0, 0]
+	if color_ray1.is_colliding():
+		if color_ray1.get_collider().name == "parcoursBody":
+			detection[0] = 1
+	if color_ray2.is_colliding():
+		if color_ray2.get_collider().name == "parcoursBody":
+			detection[1] = 1
+	if color_ray3.is_colliding():
+		if color_ray3.get_collider().name == "parcoursBody":
+			detection[2] = 1
+	return detection
