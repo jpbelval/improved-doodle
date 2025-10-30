22b4ba3c11830c24a51cefe2ecd28a12d7eee7c9
JP <jp@belval.org>
2025-10-30 10:32:10 -0400

    ajout filtres v1

--- filtre/main.py
+++ filtre/main.py
@@ -13,17 +13,49 @@ def cheby1(fe, wp, ws, gpass, gstop):
     b, a = signal.cheby1(N, gpass, Wn=Wn, btype='lowpass', fs=fe)
     return b, a, N
 
+def cheby2(fe, wp, ws, gpass, gstop):
+    N, Wn = signal.cheb2ord(wp, ws, gpass, gstop, fs=fe)
+    b, a = signal.cheby2(N, gstop, Wn=Wn, btype='lowpass', fs=fe)
+    return b, a, N
 
-def filtre():
-    fe = 1600
-    wp = 500.0
-    ws = 750.0
-    gpass = 0.2
-    gstop = 60.0
+def filtreButter():
+    fe = 40
+    wp = 1
+    ws = 2
+    gpass = 0.9
+    gstop = 40.0
     bb, ba, bN = butter(fe, wp, ws, gpass, gstop)
-    cb, ca, cN = cheby1(fe, wp, ws, gpass, gstop)
+    print(bN)
+    zplane(bb, ba)
+    w, h = signal.freqz(bb, ba)
+    plt.figure()
+    plt.plot(w, 20*np.log10(abs(h)))
+    plt.title('Butter')
+    plt.xlabel('rad/ech')
+    plt.ylabel('gain (dB)')
+    plt.show()
+
+def filtreCheby2():
+    fe = 40
+    wp = 1.9
+    ws = 2
+    gpass = 0.9
+    gstop = 40.0
+    bb, ba, bN = cheby2(fe, wp, ws, gpass, gstop)
+    print(bN)
+    zplane(bb, ba)
+    w, h = signal.freqz(bb, ba)
+    plt.figure()
+    plt.plot(w, 20*np.log10(abs(h)))
+    plt.title('Cheby2')
+    plt.xlabel('rad/ech')
+    plt.ylabel('gain (dB)')
+    plt.show()
+    
+
 
 
 
 if __name__ == '__main__':
-    filtre()
+    filtreButter()
+    filtreCheby2()
