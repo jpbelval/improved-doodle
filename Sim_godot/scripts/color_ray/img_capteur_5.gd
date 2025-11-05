46b5813ef869800fec4326a4800544bb8b599ed3
MAubin <63813705+maubin03@users.noreply.github.com>
2025-11-05 09:07:09 -0500

    UI des capteurs

---
+++ Sim_godot/scripts/color_ray/img_capteur_5.gd
@@ -0,0 +1,18 @@
+extends TextureRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var raycast_node = get_node("/root/Main/voiture/ColorRay5")
+	raycast_node.target_detected.connect(_on_target_detected)
+
+func _on_target_detected(hit: bool):
+	if hit == true:
+		texture = load("res://assets/img/red-circle.png")
+	else:
+		texture = load("res://assets/img/gray-circle.png")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
