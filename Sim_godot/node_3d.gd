80345131ba714f1be4c2a169d5f1ceca69e5a282
Laurent Brochu <laurentbrochu2003@gmail.com>
2025-12-02 10:37:14 -0500

    ip

--- Sim_godot/node_3d.gd
+++ Sim_godot/node_3d.gd
@@ -62,7 +62,7 @@ var lineValue
 func _init():
 	print("Connecting…")
 	ws = WebSocketPeer.new()
-	ws.connect_to_url("ws://10.0.0.175:8765")  # Adresse du PiCar
+	ws.connect_to_url("ws://192.168.2.104:8765")  # Adresse du PiCar
 	
 	# Private
 	movementSpeed = 0.0 # m/s
