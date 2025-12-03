e164fd04b7d6ed43acc5f0ba756c54f1142a1e6a
jeouel <jerome.ouellette@gmail.com>
2025-12-03 14:08:00 -0500

    weeeeeee

--- Sim_godot/node_3d.gd
+++ Sim_godot/node_3d.gd
@@ -171,9 +171,7 @@ func fastStateMachine(delta: float) -> void:
 		-4: # Complete stop
 			speedTarget = 0.0
 			wheelAngleTarget = 0.0
-			acceleration = 0.0
 			state = -4
-			accelerationTarget = 0.0
 			
 		-3: # Wait for next state
 			timer += delta
@@ -212,6 +210,7 @@ func fastStateMachine(delta: float) -> void:
 				state = -4
 				accelerationTarget = dodgeAcc
 				acceleration = accelerationTarget
+				speedTarget = 0.0
 				timer = 0.0
 			elif !lineValue && timer > 1.5:
 				state = -3 # figurer quel state mettre
@@ -251,7 +250,7 @@ func fastStateMachine(delta: float) -> void:
 			nextState = 3
 			setWheelAngle(avoidance_direction, 20)
 			speedTarget = midSpeed
-			delay = 3
+			delay = 4
 		
 		3: # parall elize peopele
 			timer += delta
