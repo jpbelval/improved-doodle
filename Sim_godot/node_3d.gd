e83c5dca762863f5dfb5521864091070cfa7e8e6
jeouel <jerome.ouellette@gmail.com>
2025-12-04 09:41:13 -0500

    dnijAWB

--- Sim_godot/node_3d.gd
+++ Sim_godot/node_3d.gd
@@ -212,7 +212,7 @@ func fastStateMachine(delta: float) -> void:
 				wheelAngleTarget = 0.0
 				acceleration = accelerationTarget
 				timer = 0.0
-			elif !lineValue && timer > 1:
+			elif !lineValue && timer > 0.9:
 				state = -3 # figurer quel state mettre
 				timer = 0.0
 				if wheelAngleTarget > 0:
