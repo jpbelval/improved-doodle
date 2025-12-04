54cba67e952d6f2eceba0e83188d61101833be4f
jeouel <jerome.ouellette@gmail.com>
2025-12-04 13:12:13 -0500

    circuit mystere

--- Sim_godot/node_3d.gd
+++ Sim_godot/node_3d.gd
@@ -212,7 +212,7 @@ func fastStateMachine(delta: float) -> void:
 				wheelAngleTarget = 0.0
 				acceleration = accelerationTarget
 				timer = 0.0
-			elif !lineValue && timer > 1.0:
+			elif !lineValue && timer > 0.45:
 				state = -3 # figurer quel state mettre
 				timer = 0.0
 				if wheelAngleTarget > 0:
